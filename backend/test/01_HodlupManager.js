ef0cb33de946661147d21021ac9e0e8d7a66b6e0
ytison <35665587+ytison@users.noreply.github.com>
2023-10-25 22:22:49 +0200

    init hodlupManager test

---
+++ backend/test/01_HodlupManager.js
@@ -0,0 +1,24 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers')
+const { expect } = require('chai')
+const { BigNumber } = require('ethers')
+const { anyValue } = require('@nomicfoundation/hardhat-chai-matchers/withArgs');
+const Constant = require("./lib/Constants.js")
+const Fixture = require("./lib/Fixtures.js")
+
+describe('HodlupManager', function () {
+
+    describe("test constructor", function () {
+        it("should initialize", async () => {
+        });
+    });
+
+    describe("test createDcaPaire", function () {
+    });
+
+    describe("test withdrawAllFees", function () {
+    });
+
+    describe("test executeAllSwap", function () {
+    });
+    
+});
